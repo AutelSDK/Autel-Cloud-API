99b1642b2aeba49de802e6935a27f0c3dea9a700
R22647 <R22647@autel.com>
2025-12-19 09:14:21 +0800

    道通航空上云修改

--- cloud-service/src/main/java/com/autel/service/control/model/enums/DroneAuthorityEnum.java
+++ cloud-service/src/main/java/com/autel/service/control/model/enums/DroneAuthorityEnum.java
@@ -2,11 +2,7 @@ package com.autel.service.control.model.enums;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 
-/**
- * @author sean
- * @version 1.4
- * @date 2023/3/1
- */
+
 public enum DroneAuthorityEnum {
 
     FLIGHT(1), PAYLOAD(2);
